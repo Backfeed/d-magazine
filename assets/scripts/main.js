394a362e76f702ac69b471c8d34046222127a64f
Ore Landau <Ore4444@gmail.com>
2016-02-10 17:48:14 +0200

    changed ui. disabled resizing.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,7 @@
 let getParameterByName = (name, url = window.location.href) => {
     name = name.replace(/[\[\]]/g, "\\$&");
-
     let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
-
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
@@ -15,22 +13,10 @@ if (getParameterByName('referrer')) {
 
 document.addEventListener("DOMContentLoaded", () => {
     let resizable = document.getElementById('backfeed-collabar'),
-        resizer = document.getElementById('resizer'),
-        startY,
-        startHeight,
-        initDrag = e => {
-            startY = e.clientY;
-            startHeight = parseInt(getComputedStyle(resizable, null).height, 10);
-            document.documentElement.addEventListener('mousemove', doDrag, false);
-            document.documentElement.addEventListener('mouseup', stopDrag, false);
-        },
-        doDrag = e => {
-            resizable.style.height = (startHeight - e.clientY + startY) + 'px';
-        },
-        stopDrag = e => {
-            document.documentElement.removeEventListener('mousemove', doDrag, false);
-            document.documentElement.removeEventListener('mouseup', stopDrag, false);
-        };
+        resizer = document.getElementById('backfeed-collabar-more');
 
-    resizer.addEventListener('mousedown', initDrag, false);
+    resizer.addEventListener('click', () => {
+        let startHeight = parseInt(getComputedStyle(resizable, null).height, 10);
+        resizable.style.height = (startHeight + 100) + 'px';
+    }, false);
 });
--- templates/collabar.php
+++ templates/collabar.php
@@ -1,28 +1,27 @@
-<aside id="backfeed-collabar">
-    <div id="resizer"></div>
-    <section>
-        <button id="backfeed-collabar-learn">Learn More</button>
-        <i class="bf-fa bf-fa-arrow-up"></i>
-        <span>87</span>
-        <i class="bf-fa bf-fa-arrow-down"></i>
-        <button>Share</button>
-    </section>
 <?php if (is_single()): ?>
-    <hr />
+<aside id="backfeed-collabar">
     <section>
-        <ul class="list-inline text-center">
-            <li><a href="https://www.facebook.com/sharer/sharer.php?u=<?=urlencode(get_referral_url())?>" target="_blank"><span class="bf-fa bf-fa-facebook"></span></a></li>
-            <li><a href="https://twitter.com/home?status=<?=urlencode(get_the_title() .' - '. get_referral_url())?>" target="_blank"><span class="bf-fa bf-fa-twitter"></span></a></li>
-            <li><a href="https://plus.google.com/share?url=<?=urlencode(get_referral_url())?>" target="_blank"><span class="bf-fa bf-fa-google-plus"></span></a></li>
-            <li><a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?=urlencode(get_referral_url())?>&amp;title=<?=urlencode(get_the_title())?>&amp;summary=<?=urlencode(get_the_excerpt())?>&amp;source=<?php esc_attr(get_bloginfo('name')); ?>" target="_blank"><span class="bf-fa bf-fa-linkedin"></span></a></li>
-        </ul>
+        <button id="backfeed-collabar-more">More</button>
+        <div id="backfeed-collabar-voting">
+            <i class="bf-fa bf-fa-arrow-up"></i>
+            <span>87</span>
+            <i class="bf-fa bf-fa-arrow-down"></i>
+        </div>
+        <div id="backfeed-collabar-sharing">
+            <a href="https://www.facebook.com/dialog/share?app_id=145634995501895&display=popup&href=<?=urlencode(get_referral_url())?>" target="_blank"><span class="bf-fa bf-fa-facebook"></span></a>
+            <a href="https://twitter.com/home?status=<?=urlencode(get_the_title() .' - '. get_referral_url())?>" target="_blank"><span class="bf-fa bf-fa-twitter"></span></a>
+        </div>
     </section>
-<?php endif; ?>
-    <hr />
+    <section>
+        <p>Microcopy</p>
+        <a href="#">Learn More</a>
+    </section>
     <section>
         <?php wp_loginout($_SERVER['REQUEST_URI']); ?>
+
         <?php if (!is_user_logged_in()): ?>
         <a href="<?=esc_url(wp_registration_url())?>">Register</a>
         <?php endif; ?>
     </section>
 </aside>
+<?php endif; ?>
