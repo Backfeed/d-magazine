fae06fc13ace923fe367a98a315f0e2cf048e2c3
Ore Landau <Ore4444@gmail.com>
2016-02-16 13:46:08 +0200

    improved uninstall script

--- uninstall.php
+++ uninstall.php
@@ -1,3 +1,6 @@
 <?php
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
+    exit();
+
 delete_metadata('user', 0, 'backfeed_user_id', '', true);
 delete_metadata('post', 0, 'backfeed_contribution_id', '', true);
