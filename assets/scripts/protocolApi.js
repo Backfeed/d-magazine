21230ec28b0d23979055f9e567c61535be3c8cb1
Ore Landau <Ore4444@gmail.com>
2016-02-16 18:58:27 +0200

    started evaluations implementation

--- assets/scripts/protocolApi.js
+++ assets/scripts/protocolApi.js
@@ -1,14 +1,19 @@
+const apiKey = 'cU1pjBJDBP1KsHgbVBwO99F02DvWWR9S62kkFGzQ';
+
+/* TODO: get from localize script */
+const biddingId = 'c5d77b3d-a56b-43cb-9e99-99be32b822e8';
+
 let protocolApi = {
     request: (endpoint, options, headers) => {
         let apiUrl = 'https://api.backfeed.cc/dev/',
             defaultHeaders = {
-                'X-Api-Key': 'cU1pjBJDBP1KsHgbVBwO99F02DvWWR9S62kkFGzQ'
+                'X-Api-Key': apiKey
             },
             defaultOptions = {
                 headers: new Headers(_.extend(defaultHeaders, headers))
             };
 
-        return fetch(apiUrl + endpoint, _.extend(defaultOptions, options))
+        return fetch(apiUrl+endpoint, _.extend(defaultOptions, options))
             .then(res => res.json());
     },
     createUser: () => {
@@ -16,6 +21,19 @@ let protocolApi = {
     },
     getUserById: (userId) => {
         return protocolApi.request('users/'+userId)
+    },
+    evaluate: (value, userId, contributionId) => {
+        return protocolApi.request('evaluations/submit', {
+            method: 'POST',
+            body: JSON.stringify({
+                userId,
+                biddingId,
+                evaluations: [{
+                    contributionId,
+                    value
+                }]
+            })
+        });
     }
 
 };
