6fc6c89878e488fb904e3e37d74f2386895109d6
Ore Landau <Ore4444@gmail.com>
2016-02-24 11:48:05 +0200

    styling touches to logged out

--- templates/collabar-guest.php
+++ templates/collabar-guest.php
@@ -12,7 +12,8 @@
 			<a class="button" href="<?=esc_url(wp_registration_url())?>">REGISTER</a>
 		</div>
 		<div class="backfeed-collabar-tagline"><span>We are</span><br>crowdsourced</div>
-		<div class="backfeed-collabar-learn">Register to participate, get compensated for your contribution</div>
+		<div class="backfeed-collabar-learn hidden-lg-up">Register to participate, get compensated for your contribution</div>
+		<div class="backfeed-collabar-learn hidden-md-down">Everything you see here is edited by our collective intelligence.<br />Register to participate, and get compensated for your contribution.</div>
 		<button class="button backfeed-collabar-learn-button" id="backfeed-collabar-more">Learn more</button>
 	</section>
 </aside>
