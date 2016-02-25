047daa5ed1ec164679c6061702f5a6bbf3842f7c
Ore Landau <Ore4444@gmail.com>
2016-02-25 14:56:50 +0200

    finished logged-in non-article

--- templates/collabar-guest.php
+++ templates/collabar-guest.php
@@ -12,8 +12,8 @@
 			<a class="button" href="<?=esc_url(wp_registration_url())?>">REGISTER</a>
 		</div>
 		<div class="backfeed-tagline"><span>We are</span><br>crowdsourced</div>
-		<div class="backfeed-learn hidden-lg-up">Register to participate, get compensated for your contribution</div>
-		<div class="backfeed-learn hidden-md-down">Everything you see here is edited by our collective intelligence.<br />Register to participate, and get compensated for your contribution.</div>
+		<div class="backfeed-learn hidden-xl-up">Register to participate, get compensated for your contribution</div>
+		<div class="backfeed-learn hidden-lg-down">Everything you see here is edited by our collective intelligence.<br />Register to participate, and get compensated for your contribution.</div>
 		<button class="button backfeed-learn-button">Learn more</button>
 	</section>
 </aside>
--- templates/collabar-user.php
+++ templates/collabar-user.php
@@ -1,8 +1,7 @@
 <aside id="backfeed-collabar">
-    <?php if (is_single()): ?>
-        
-        <section>
-            <button id="backfeed-more">More</button>
+
+    <section>
+        <?php if (is_single()): ?>
             <div id="backfeed-voting">
                 <i class="bf-fa bf-fa-arrow-up"></i>
                 <span>87</span>
@@ -12,64 +11,47 @@
                 <a href="https://www.facebook.com/dialog/share?app_id=145634995501895&display=popup&href=<?=urlencode(get_referral_url())?>" target="_blank"><span class="bf-fa bf-fa-facebook"></span></a>
                 <a href="https://twitter.com/home?status=<?=urlencode(get_the_title() .' - '. get_referral_url())?>" target="_blank"><span class="bf-fa bf-fa-twitter"></span></a>
             </div>
-        </section>
+        <?php endif; ?>
 
-        <section>
-            <p>Rate, share or comment on this article in order to get tokens</p>
-            <a href="#">Learn More</a>
-        </section>
+        <p class="backfeed-teaser">
+            <span class="hidden-sm-up">Be part of the collective intelligence and earn tokens!</span>
+            <span class="hidden-xs-down">Explore our magazine, be part of the collective intelligence and earn tokens!</span>
+            &nbsp;&nbsp;<button class="button backfeed-learn-button">Learn more</button>
+        </p>
 
-        <section>
-            <div class="backfeed-stats">
-                <div class="backfeed-stat">
-                    <label>Tokens</label>  <span>25</span>
-                </div>
-                <div class="backfeed-stat">
-                    <label>Reputation</label>  <span>72</span>
+        <div class="backfeed-user-info">
+
+            <div class="backfeed-stats hidden-sm-down backfeed-tooltip">
+                <div class="backfeed-stat"><label>My Tokens:</label> 41</div>
+                <div class="backfeed-stat"><label>Rank:</label> NOVICE (0.12%)</div>
+                <div class="backfeed-tooltip-content">
+                    <p>Reputation represents your influence on the editing process</p>
+                    <p>Tokens are your share of the value we’ve created together</p>
                 </div>
             </div>
 
-            <?php wp_loginout($_SERVER['REQUEST_URI']); ?>
-        </section>
-
-    <?php else: ?>
-
-        <section>
-
-            <p class="backfeed-teaser">Be part of the collective intelligence and earn tokens!
-                <button class="button backfeed-learn-button">Learn more</button>
-            </p>
-
-            <div class="backfeed-user-info">
-                
-                <div class="backfeed-stats hidden-sm-down">
-                    <div class="backfeed-stat"><label>Rank:</label> NOVICE (0.12%)</div>
-                    <div class="backfeed-stat"><label>My Tokens:</label> 41</div>
-                </div>
-
-                <div id="backfeed-avatar" class="backfeed-avatar">
-                    <?=get_avatar(wp_get_current_user()->ID, 32)?>
-                    <i class="bf-fa bf-fa-caret-down"></i>
-                </div>
-
+            <div id="backfeed-avatar" class="backfeed-avatar">
+                <?=get_avatar(wp_get_current_user()->ID, 32)?>
+                <i class="bf-fa bf-fa-caret-down"></i>
             </div>
 
-        </section>
+        </div>
+
+    </section>
 
 
-        <section class="hidden-md-up">
-            <div><label>Rank:</label> NOVICE (0.12%)</div>
-            <div><label>My Tokens:</label> 41</div>
-        </section>
+    <section class="hidden-md-up">
+        <div><label>Rank:</label> NOVICE (0.12%)</div>
+        <div><label>My Tokens:</label> 41</div>
+    </section>
 
-        <nav id="backfeed-avatar-menu" class="backfeed-avatar-menu">
-            <ul>
-                <li><?php wp_loginout($_SERVER['REQUEST_URI']); ?></li>
-                <li><a href="<?=get_edit_profile_url()?>">Edit Profile</a></li>
-                <li class="backfeed-disabled">Statistics</li>
-                <li><a href="<?=get_permalink(396)?>">Submit an Article</a></li>
-            </ul>
-        </nav>
+    <nav id="backfeed-avatar-menu" class="backfeed-avatar-menu">
+        <ul>
+            <li><?php wp_loginout($_SERVER['REQUEST_URI']); ?></li>
+            <li><a href="<?=get_edit_profile_url()?>">Edit Profile</a></li>
+            <li class="backfeed-disabled">Statistics</li>
+            <li><a href="<?=get_permalink(396)?>">Submit an Article</a></li>
+        </ul>
+    </nav>
 
-    <?php endif; ?>
 </aside>
